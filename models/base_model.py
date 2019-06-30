e43af069aaa5946b5d5b278676c4592f8c697aa9
arleybri18 <arleybri18@gmail.com>
2019-06-30 04:01:23 +0000

    advanced in task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Class BaseModel"""
 import uuid
+from models import storage
 from datetime import datetime
 
 
@@ -20,11 +21,13 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def save(self):
         """ updates the public instance attribute updated_at
         with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from os.path import exists
+from json import dump, load, dumps
+
+
+class FileStorage:
+    __file_path = "objects.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        class_name = obj.__class__.__name__
+        iden = obj.id
+        cl_id = class_name + "." + iden
+        FileStorage.__objects[cl_id] = obj.to_dict()
+
+    def save(self):
+        dict_to_json = {}
+        for key, value in FileStorage.__objects.items():
+            dict_to_json[key] = value
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as fil:
+            dump(dict_to_json, fil)
+
+    def reload(self):
+        if (exists(FileStorage.__file_path)):
+            with open(FileStorage.__file_path, "r") as fil:
+                FileStorage.__objects = load(fil)
