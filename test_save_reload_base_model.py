6537b1371eac7d5341b80a8397c77ef6a6a8e732
arleybri18 <arleybri18@gmail.com>
2019-07-01 20:10:29 +0000

    change mode in the open file to w write in to the save method

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models import storage
 from models.base_model import BaseModel
-
+#import pdb; pdb.set_trace()
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
