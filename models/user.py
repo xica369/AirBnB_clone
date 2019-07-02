132542e49373204abbc64ed0dc8c4587daf975ef
arleybri18 <arleybri18@gmail.com>
2019-07-02 01:03:17 +0000

    create class user, modify file_storage, fixes reload

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Class User for create users of application """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ init method call method of the super class """
+        """instantiates a new user"""
+        super().__init__(self, *args, **kwargs)
