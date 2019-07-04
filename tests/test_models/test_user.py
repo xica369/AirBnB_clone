a57b6d5e93dc9ee8ac4c94ac823091172c9fa02d
arleybri18 <arleybri18@gmail.com>
2019-07-04 04:51:19 +0000

    fixes test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,79 +1,95 @@
 #!/usr/bin/python3
-"""This module tests all about User """
-from models.user import User
+""" unit test for class BaseModel """
+
+import models
 import unittest
-import os
-from datetime import datetime
-import pep8
+
+BaseModel = models.base_model.BaseModel
+User = models.user.User
 
 
-class testUser(unittest.TestCase):
-    ''' Test of the model BaseModel '''
+class TestBaseModelDocs(unittest.TestCase):
+    """ validate docstring in the class """
 
+    def test_doc_module(self):
+        """ validate documentation module """
+        doc = models.user.__doc__
+        assert doc is not None
+
+    def test_doc_class(self):
+        """ validate documentation class """
+        doc = User.__doc__
+        assert doc is not None
+
+    def test_doc_methods_class(self):
+        """ validate documentation methods """
+        l_meth = ["save", "__init__", "__str__", "to_dict"]
+        for key in User.__dict__.keys():
+            if key is l_meth:
+                doc = key.__doc__
+                assert doc is not None
+
+
+class TestBaseModelInstances(unittest.TestCase):
+    """ validate creation objects and use methods """
     @classmethod
     def setUpClass(cls):
-        ''' test_inst up '''
-        cls.test_inst = User()
-        cls.test_inst.first_name = "Betty"
-        cls.test_inst.last_name = "Holberton"
-        cls.test_inst.email = "airbnb@holberton.com"
-        cls.test_inst.password = "root"
-        cls.test_inst.save()
-        cls.test_inst_str = cls.test_inst.to_dict()
+        ''' new_user up '''
+        cls.new_user = User()
+        cls.new_user.first_name = "Betty"
+        cls.new_user.last_name = "Holberton"
+        cls.new_user.email = "airbnb@holberton.com"
+        cls.new_user.password = "root"
+        cls.new_user.save()
+        cls.new_user_str = cls.new_user.to_dict()
 
-    def test_isInstance(self):
-        ''' test if is a instance of BaseModel '''
-        self.assertIsNot(self.test_inst, User)
-        self.assertTrue(isinstance(self.test_inst, User))
+    def test_create_object(self):
+        """ validate created instance """
+        self.assertIsInstance(self.new_user, User)
 
-    def test_attrib(self):
-        ''' test the attributes created '''
-        self.assertEqual(self.test_inst_str['first_name'], 'Betty')
-        self.assertEqual(self.test_inst_str['last_name'], 'Holberton')
-        self.assertEqual(self.test_inst_str['email'], 'airbnb@holberton.com')
-        self.assertEqual(self.test_inst_str['password'], 'root')
+    def test_string_representation(self):
+        """ validate string representation """
+        rep_str = str(self.new_user)
+        list_att = ['User', 'id', 'created_at', 'updated_at']
+        num_att = 0
+        for att in list_att:
+            if att in rep_str:
+                num_att += 1
+        self.assertTrue(4 == num_att)
 
-    def test_typeAttrib(self):
-        ''' test the type of the attributes in the dict '''
-        self.assertEqual(type(self.test_inst_str['created_at']), str)
-        self.assertEqual(type(self.test_inst_str['updated_at']), str)
-
-    def test_saveMethod(self):
-        ''' test the method save works '''
-        temp = self.test_inst.updated_at
-        self.test_inst.save()
-        self.assertNotEqual(temp, self.test_inst.updated_at)
+    def test_method_save(self):
+        """ validate save method """
+        current = self.new_user.updated_at
+        self.new_user.save()
+        new = self.new_user.updated_at
+        self.assertNotEqual(current, new)
 
     def test_hasMethods(self):
         ''' test the instance have the methods  '''
-        self.assertTrue(hasattr(self.test_inst, '__str__'))
-        self.assertTrue(hasattr(self.test_inst, '__init__'))
-        self.assertTrue(hasattr(self.test_inst, 'to_dict'))
-        self.assertTrue(hasattr(self.test_inst, 'save'))
+        self.assertTrue(hasattr(self.new_user, '__str__'))
+        self.assertTrue(hasattr(self.new_user, '__init__'))
+        self.assertTrue(hasattr(self.new_user, 'to_dict'))
+        self.assertTrue(hasattr(self.new_user, 'save'))
 
-    def test_hasDocumentation(self):
-        ''' test the methods have documentation '''
-        self.assertIsNotNone(self.test_inst.__str__.__doc__)
-        self.assertIsNotNone(self.test_inst.save.__doc__)
+    def test_add_attributes(self):
+        """ add attributes to object"""
+        self.new_user.first_name = "Jhon"
+        self.new_user.last_name = "Doe"
+        list_att = [self.new_user.first_name, self.new_user.last_name]
+        expected = ["Jhon", "Doe"]
+        self.assertEqual(expected, list_att)
 
-    def test_pep8_conformance(self):
-        ''' Test that we conform to PEP8 '''
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files([
-                                        'models/user.py',
-                                        'tests/test_models/test_user.py'
-                                        ])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def test_method_to_dict(self):
+        self.new_user.first_name = "Jhon"
+        self.new_user.last_name = "Doe"
+        dict_rep = self.new_user.to_dict()
+        list_att = ['id', 'created_at', 'updated_at',
+                    'first_name', 'last_name', '__class__']
+        num_att = 0
+        for att in dict_rep.keys():
+            if att in list_att:
+                num_att += 1
+        self.assertTrue(6 == num_att)
 
-    @classmethod
-    def tearDownClass(cls):
-        ''' test_inst Down '''
-        del cls.test_inst
-        try:
-            os.remove("file.json")
-        except BaseException:
-            pass
-
-if __name__ == "__main__":
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
